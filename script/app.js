471d3a6a0be904bb42802c409e9df12014248888
Takaitech <takai.tech2144@gmail.com>
2019-04-18 15:58:34 -0700

    scrollify

--- script/app.js
+++ script/app.js
@@ -3,15 +3,4 @@ $(function() {
       section : ".page",
       scrollSpeed: 2000,
     });
-
-    $(document.body).on('touchmove',function(e){
-      e.preventDefault();
-      $.scrollify.next();
-      //CODE GOES HERE
-    });
-
-    $(".page").scroll(function() { //.box is the class of the div
-      $.scrollify.next();
-  });
-  
   });
