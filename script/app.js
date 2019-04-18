694d718e417bd20543f91cd8d05d2d4d12808a80
Takaitech <takai.tech2144@gmail.com>
2019-04-18 14:56:37 -0700

    integrated jquery and jquery scrollify plugin

---
+++ script/app.js
@@ -0,0 +1,31 @@
+$.scrollify({
+    section : "section",
+    sectionName : "section-name",
+    interstitialSection : "",
+    easing: "easeOutExpo",
+    scrollSpeed: 1100,
+    offset : 0,
+    scrollbars: true,
+    standardScrollElements: "",
+    setHeights: true,
+    overflowScroll: true,
+    updateHash: true,
+    touchScroll:true,
+    before:function() {},
+    after:function() {},
+    afterResize:function() {},
+    afterRender:function() {}
+  });
+        
+
+  $(function() {
+    $.scrollify({
+      section : ".page",
+    });
+
+
+    $("#sections").scroll(function() { //.box is the class of the div
+      $.scrollify.next();
+  });
+  
+  });
