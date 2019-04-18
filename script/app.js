66dace8be9c2ff2a5e7c563e6aa00304435d9e9c
Takaitech <takai.tech2144@gmail.com>
2019-04-18 16:18:56 -0700

    scrollify mobile scroll attempt 2

--- script/app.js
+++ script/app.js
@@ -1,6 +1,19 @@
 $(function() {
-    $.scrollify({
-      section : ".page",
-      scrollSpeed: 2000,
-    });
-  });
+
+
+	$.scrollify({
+		section:".page",
+		//sectionName:false,
+		scrollSpeed:1100,
+		after:function(i) {
+    }
+	});
+
+	$(".scroll,.scroll-btn").click(function(e) {
+		e.preventDefault();
+
+		$.scrollify.next();
+
+
+	});
+});
