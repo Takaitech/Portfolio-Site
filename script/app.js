a89967bcf0c71aca2acee7b67b3df03997d824ae
Takaitech <takai.tech2144@gmail.com>
2019-04-29 23:40:39 -0700

    css styling/animations

--- script/app.js
+++ script/app.js
@@ -1,5 +1,24 @@
 $(function() {
     $.scrollify({
-        section: ".page"
+        section: ".page",   
+    before: function(section) {
+        console.log(section)
+        if(section === 1) {
+            $(".section2Description").addClass("fadeInLeftBig")
+            $(".section2Image").addClass("fadeInRightBig")
+            $(".section2Category").addClass("fadeInLeftBig")
+        }
+        if(section !== 1) {
+
+            $(".section2Description").removeClass("fadeInLeftBig")
+            $(".section2Image").removeClass("fadeInRightBig")
+            $(".section2Category").removeClass("fadeInLeftBig")
+
+        }
+        if(section === 2) {
+        }
+        if(section !== 2) {
+        }
+    }
     })
 })
